07660417ceb163b6d6c3803d57461c42af5f8316
CruelSun0 <jailsonsunshime@gmail.com>
2025-10-15 14:51:41 -0300

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+import styles from "./page.module.css";
+import Drawer from "components/Drawer";
+
+export default function Home() {
+
+  
+
+  return (
+    <div className={styles.page}>
+     <Drawer/>
+    </div>
+  );
+}
