358fe27e9d9d202e22899a2e52cb6ae82acb128d
CruelSun0 <jailsonsunshime@gmail.com>
2025-10-25 18:52:35 -0300

    Pequenas correções em Product e Colors sections

--- src/components/SectionColors/SectionColors.tsx
+++ src/components/SectionColors/SectionColors.tsx
@@ -1,6 +1,10 @@
 import styles from './SectionColors.module.css' 
 import { useState } from 'react';
-import { HiArrowRight } from 'react-icons/hi';
+import { HiArrowRight, HiOutlineArrowDown } from 'react-icons/hi';
+import { ContactShadows, Environment, Lightformer, OrbitControls } from '@react-three/drei';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import Ferrari from '../FerrariModel/FerrariModel';
 
 const arrayCars = [
 
@@ -20,7 +24,51 @@ const SectionColors = () => {
     return (
     <section 
     id="#colors" className={styles.colors}>
-        
+            <div className={styles.model3dContent}>
+            
+        <Canvas shadows gl={{antialias:false}}
+      camera={{position:[2,0,-5], fov:50}}>
+        <ambientLight intensity={0.2}  position={[1, 10, 10]}/>
+        <hemisphereLight intensity={0.2}/>
+        <mesh scale={4} position={[3, -1.161, -1.5]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
+        <ringGeometry args={[0.9, 1, 4, 1]} />
+        <meshStandardMaterial color="white" roughness={0.75} />
+        </mesh>
+        <mesh scale={4} position={[-3, -1.161, -1]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
+        <ringGeometry args={[0.9, 1, 3, 1]} />
+        <meshStandardMaterial color="white" roughness={0.75} />
+        </mesh>
+        <directionalLight castShadow={true} position={[0, 10, 10]}
+        intensity={1.5} shadow-mapSize={[2048, 2048]}/>
+        <Suspense fallback={null}>
+        <Ferrari position={[0, -1.2, 0]} rotation={[0, Math.PI / 1.5, 0]} scale={1.2}/>
+        <color attach="background" args={['#15151a']}/>
+        </Suspense>
+        <Environment resolution={512} >
+        {/* Ceiling */}
+        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} />
+        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} />
+        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} />
+        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} />
+        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} />
+        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
+        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
+        {/* Sides */}
+        <Lightformer intensity={0.5} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
+        <Lightformer intensity={0.5} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
+        {/* Key */}
+        <Lightformer form="ring" color="red" intensity={10} scale={2} position={[10, 5, 10]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
+      </Environment>
+        <OrbitControls maxPolarAngle={1.5} autoRotate rotateSpeed={0.3}
+        enableDamping={true}/>
+        <ContactShadows frames={1} opacity={1} scale={500} 
+        blur={1.2} far={1000} resolution={1024}/>
+      </Canvas>
+      <div className={styles.feedbackForUser}>
+        <span>Click to scroll</span>
+        <HiOutlineArrowDown size={16} className={styles.arrowIcon}/>
+      </div>
+      </div>
         <div className={styles.colorsContent} >
             <p className={styles.titleColors}>Escolha sua cor:</p>
             <img 
--- src/components/SectionProduct/SectionProduct.tsx
+++ src/components/SectionProduct/SectionProduct.tsx
@@ -1,16 +1,13 @@
 "use client";
 import styles from './SectionProduct.module.css'
-import { ContactShadows, Environment, Lightformer, OrbitControls } from '@react-three/drei';
-import { Suspense } from 'react';
-import Ferrari from '../FerrariModel/FerrariModel';
-import { Canvas } from '@react-three/fiber';
-import { HiOutlineArrowDown, HiOutlineCurrencyDollar } from 'react-icons/hi';
+import { HiOutlineCurrencyDollar } from 'react-icons/hi';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap'
 import { ScrollTrigger, TextPlugin } from 'gsap/all';
 import Card from '../Card/Card';
 import { FiCrosshair, FiTarget, FiTool, FiWind, FiZap } from 'react-icons/fi';
-import * as THREE from 'three'
+import Image from 'next/image';
+import { HiOutlineArrowRight } from 'react-icons/hi2';
 
 
 gsap.registerPlugin(useGSAP)
@@ -30,7 +27,7 @@ const SectionProduct = () => {
           markers:true,
           trigger:`.${styles.screensContent}`,
           start:`top top`,
-          end:`+=500%`,
+          end:`+=1000%`,
           scrub:1,
           snap:1/6,
           pin:true,
@@ -52,35 +49,35 @@ const SectionProduct = () => {
         text:'— Ícone da virada de milênio',
         ease:"none",
 
-      }, 1)
+      }, 2)
 
      
 
       tl.fromTo(`.${styles.firstScreen}`, 
         {translateX:'-101%'},
         {translateX:0},
-       2
+        
 
       )
 
       tl.fromTo(`.${styles.secondScreen}`, 
         {translateX:'101%'},
         {translateX:0},
-        3
+       10
 
       )
 
       tl.fromTo(`.${styles.thirdScreen}`, 
         {translateY:'-101%'},
         {translateY:0},
-        4
+        14
 
       )
 
       tl.fromTo(`.${styles.fourthScreen}`, 
         {translateY:'101%'},
         {translateY:0},
-        5
+        18
 
       )
     })
@@ -88,51 +85,18 @@ const SectionProduct = () => {
     return (
 
         <section id="product" className={styles.product}>
-        <div className={styles.model3dContent}>
-            
-        <Canvas shadows gl={{antialias:false}}
-      camera={{position:[2,0,-5], fov:50}}>
-        <ambientLight intensity={0.2}  position={[1, 10, 10]}/>
-        <hemisphereLight intensity={0.2}/>
-        <mesh scale={4} position={[3, -1.161, -1.5]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
-        <ringGeometry args={[0.9, 1, 4, 1]} />
-        <meshStandardMaterial color="white" roughness={0.75} />
-        </mesh>
-        <mesh scale={4} position={[-3, -1.161, -1]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
-        <ringGeometry args={[0.9, 1, 3, 1]} />
-        <meshStandardMaterial color="white" roughness={0.75} />
-        </mesh>
-        <directionalLight castShadow={true} position={[0, 10, 10]}
-        intensity={1.5} shadow-mapSize={[2048, 2048]}/>
-        <Suspense fallback={null}>
-        <Ferrari position={[0, -1.2, 0]} rotation={[0, Math.PI / 1.5, 0]} scale={1.2}/>
-        <color attach="background" args={['#15151a']}/>
-        </Suspense>
-        <Environment resolution={512} >
-        {/* Ceiling */}
-        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} />
-        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} />
-        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} />
-        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} />
-        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} />
-        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
-        <Lightformer intensity={0.5} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
-        {/* Sides */}
-        <Lightformer intensity={0.5} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
-        <Lightformer intensity={0.5} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
-        {/* Key */}
-        <Lightformer form="ring" color="red" intensity={10} scale={2} position={[10, 5, 10]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
-      </Environment>
-        <OrbitControls maxPolarAngle={1.5} autoRotate rotateSpeed={0.3}
-        enableDamping={true}/>
-        <ContactShadows frames={1} opacity={1} scale={500} 
-        blur={1.2} far={1000} resolution={1024}/>
-      </Canvas>
-      <div className={styles.feedbackForUser}>
-        <span>Click to scroll</span>
-        <HiOutlineArrowDown size={16} className={styles.arrowIcon}/>
-      </div>
-      </div>
+        <div className={styles.introductionCallContent}>
+
+        <Image priority fill
+        src="/car-background.jpg" alt="background" 
+        className={styles.introductionBackground}/>
+        <button className={styles.btnBuyNow}>
+
+          <span>Buy now</span>
+          <HiOutlineArrowRight size={24}/>
+        </button>
+
+        </div>
 
       <div className={styles.screensContent}>
         <div className={styles.introduction}>
